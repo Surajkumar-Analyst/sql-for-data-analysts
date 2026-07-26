d23d5914e5b9559095fd9f853ad0e74a71a0ed83
Suraj  Kumar <suraj.yadav.417330@gmail.com>
2026-07-26 20:17:11 +0530

    Time: 522 ms (39.29%), Space: 0B (100%) - LeetHub - Time: O(N^2), Space: O(N)

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,10 @@
+SELECT 
+    today.id 
+FROM Weather today
+JOIN Weather yesterday
+    ON DATEDIFF(today.recordDate, yesterday.recordDate) = 1
+WHERE today.temperature > yesterday.temperature;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
