b4b510cd902f1b56078cc803113c7a4ffef5779d
Suraj  Kumar <suraj.yadav.417330@gmail.com>
2026-07-27 21:39:48 +0530

    Time: 1223 ms (18.19%), Space: 0B (100%) - LeetHub - Time: O(n*m*log(n*m)), Space: O(n*m)

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,21 @@
+SELECT
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    COUNT(ex.student_id) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations ex
+    ON s.student_id  = ex.student_id   
+    AND sub.subject_name = ex.subject_name
+GROUP BY  
+    s.student_id,
+    s.student_name,
+    sub.subject_name
+ORDER BY
+    s.student_id,
+    sub.subject_name;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
